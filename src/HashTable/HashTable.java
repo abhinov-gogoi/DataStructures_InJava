d8246c27e8a5fbcaf99b6d2c9a9876ace9c3c013
Abhinov Gogoi <abhinov-gogoi@users.noreply.github.com>
2021-02-18 18:22:36 +0530

    testing driver code

--- src/HashTable/HashTable.java
+++ src/HashTable/HashTable.java
@@ -3,20 +3,30 @@ package HashTable;
 
 import java.util.LinkedList;
 
+/**
+     HashTable will be implemented using an array of LinkedList
+     those LinkedLists will contain objects of type Entry.
+ */
+
 public class HashTable {
     private LinkedList<Entry>[] entries = new LinkedList[5];
 
     public void put(int key, String value) {
         int index = hash(key);
-        var bucket = entries[index];
 
-        // initialise the array cell
-        // (don't replace entries[index] with bucket here)
+        /**
+             initialise the array cell
+             (don't replace entries[index] with bucket here)
+         */
         if (entries[index]==null)
             entries[index] = new LinkedList<>();
 
-        // if the unique is assigned a different value later
-        // don't add to the last, rather update the unique key with the new value
+        var bucket = entries[index];
+
+        /**
+            if the unique is assigned a different value later
+            don't add to the last, rather update the unique key with the new value
+         */
         for(var entry:bucket) {
             if(entry.key==key) {
                 entry.value = value;
@@ -37,6 +47,11 @@ public class HashTable {
         }
         return null;
     }
+
+    /**
+        Hash function takes in the key and returns the index of the array
+        at which data will be put added to the LinkedList
+     */
 
     private int hash(int key) {
         return Math.abs(key % entries.length);
--- src/HashTable/Main.java
+++ src/HashTable/Main.java
@@ -2,6 +2,11 @@ package HashTable;
 
 public class Main {
     public static void main(String[] args) {
-
+        HashTable table = new HashTable();
+        table.put(6, "Abc");
+        table.put(8, "UFY");
+        table.put(11, "HELLO");
+        table.put(6, "AAA");
+        System.out.println("Done");
     }
 }
